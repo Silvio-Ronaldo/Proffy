84dba87c4acc03de8abdfb1d4d904a96e369b97b
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2020-08-08 23:54:55 -0300

    Terminando a interface e conectando na api

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,55 @@ import React from 'react';
 
 import whatsIcon from '../../assets/images/icons/whatsapp.svg';
 
+import api from '../../services/api';
+
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+interface TeacherItemProps {
+    teacher: Teacher;     
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars0.githubusercontent.com/u/48893927?s=460&u=5a414a7b65386954ac45e948d653258d980cc7dd&v=4" alt="Silvio Ronaldo" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Silvio Ronaldo</strong>
-                    <span>Física</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Apaixonado por toda a loucura chamada Física.
-                <br /><br />
-                Há mais de 10 anos estudando e pesquisando sobre fenômenos físicos e suas consequências. Já trouxe mais de 500 alunos para esse mundo fantástico!
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>22,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target="_blank" 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsIcon} alt="WhatsApp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     );
